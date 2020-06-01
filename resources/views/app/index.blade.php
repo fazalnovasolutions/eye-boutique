5bc0e6a3fb829789a4ee64e88c965e08f6f8f6bf
Nova Solutions <fazal@novasolutions.dev>
2020-06-01 20:40:38 +0500

    live-test

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -6,11 +6,11 @@
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
                 <h1 class="flex-sm-fill h3 my-2">
-                    EyeWear-Boutique
+                    EyeWearBoutique
                 </h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb breadcrumb-alt">
-                        <li class="breadcrumb-item"> EyeWear-Boutique</li>
+                        <li class="breadcrumb-item"> EyeWearBoutique</li>
                         <li class="breadcrumb-item" aria-current="page">
                             <a class="link-fx" href="">Dashboard</a>
                         </li>
