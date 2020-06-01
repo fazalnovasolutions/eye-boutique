2b56fef49d2b9a505465ef624a49801c586e9607
Nova Solutions <fazal@novasolutions.dev>
2020-06-01 23:24:42 +0500

    162020

---
+++ app/Http/Controllers/HelperController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Osiset\BasicShopifyAPI\BasicShopifyAPI;
+
+class HelperController extends Controller
+{
+   private $api;
+   private $shop;
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
-Route::get('/dashboard', 'LenseCoatingController@index')->name('home');
+Route::group(['middleware' => ['auth.shopify']], function () {
+    Route::get('/dashboard', 'LenseCoatingController@index')->name('home');
 
-Route::post('/lens/create', 'LenseCoatingController@lens_save')->name('lens.save');
-Route::post('/lens/{id}/update', 'LenseCoatingController@lens_update')->name('lens.update');
-Route::get('/lens/{id}/delete', 'LenseCoatingController@lens_delete')->name('lens.delete');
+    Route::post('/lens/create', 'LenseCoatingController@lens_save')->name('lens.save');
+    Route::post('/lens/{id}/update', 'LenseCoatingController@lens_update')->name('lens.update');
+    Route::get('/lens/{id}/delete', 'LenseCoatingController@lens_delete')->name('lens.delete');
 
-Route::post('/coating/create', 'LenseCoatingController@coating_save')->name('coating.save');
-Route::post('/coating/{id}/update', 'LenseCoatingController@coating_update')->name('coating.update');
-Route::get('/coating/{id}/delete', 'LenseCoatingController@coating_delete')->name('coating.delete');
+    Route::post('/coating/create', 'LenseCoatingController@coating_save')->name('coating.save');
+    Route::post('/coating/{id}/update', 'LenseCoatingController@coating_update')->name('coating.update');
+    Route::get('/coating/{id}/delete', 'LenseCoatingController@coating_delete')->name('coating.delete');
 
 
-Route::get('/getLenses', 'LenseCoatingController@getLenses')->name('lenses.all');
-Route::get('/getCoatings', 'LenseCoatingController@getCoatings')->name('coatings.all');
+    Route::get('/getLenses', 'LenseCoatingController@getLenses')->name('lenses.all');
+    Route::get('/getCoatings', 'LenseCoatingController@getCoatings')->name('coatings.all');
+});
+
 
 
