ebe0f3723c479889c231ec283d5533ff282b2769
Nova Solutions <fazal@novasolutions.dev>
2020-06-01 22:23:00 +0500

    162020

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -3,6 +3,20 @@
 @include('inc.header')
 <body>
 <div id="page-container">
+    @if(config('shopify-app.appbridge_enabled'))
+        <script src="https://unpkg.com/@shopify/app-bridge{{ config('shopify-app.appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>
+        <script>
+            var AppBridge = window['app-bridge'];
+            var createApp = AppBridge.default;
+            var app = createApp({
+                apiKey: '{{ config('shopify-app.api_key') }}',
+                shopOrigin: 'eyewear-boutique-ltd.myshopify.com',
+                forceRedirect: true,
+            });
+        </script>
+
+        @include('shopify-app::partials.flash_messages')
+    @endif
     <main id="main-container">
         @include('flash_message.message')
         @yield('content')
